56b10c4e5fc08dc739d63d8d66fa2f8c0781fd76
Mahmoud Ashraf Taha <mahmoud.ash.07@gmail.com>
2023-04-10 21:55:31 +0200

    import routes to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
+import { Fragment, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RouterProvider, createBrowserRouter, redirect } from "react-router-dom";
+import NotFoundPage from "./Components/NotFoundPage/NotFoundPage";
+import Home from "./Views/Home/Home";
+import LayoutWrapper from "./UI/LayoutWrapper/LayoutWrapper";
+
 function App() {
+  const rootEle = document.getElementById("root-html");
+  const globalLang = useSelector((state) => {
+    return state.lang.globalLang;
+  });
+  useEffect(() => {
+    if (rootEle) {
+      if (globalLang === "ar") {
+        rootEle.setAttribute("dir", "rtl");
+      }
+      if (globalLang === "en") {
+        rootEle.setAttribute("dir", "ltr");
+      }
+      rootEle.setAttribute("lang", globalLang);
+      localStorage.setItem("lang", globalLang);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [globalLang]);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LayoutWrapper />,
+      children: [
+        {
+          path: '/',
+          loader: () => redirect("/home")
+        },
+        {
+          path: '/home',
+          element: <Home />
+        }
+      ],
+    },
+    {
+      path: "*",
+      loader: () => redirect("/home")
+    },
+  ]);
   return (
-    <div className="App">
-      Hello Sard
-    </div>
+    <Fragment>
+      <RouterProvider router={router} />
+    </Fragment>
   );
 }
 
