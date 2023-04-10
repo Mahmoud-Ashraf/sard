d435fe63b4342670733dfebd4bd842cbb75a2913
Mahmoud Ashraf Taha <mahmoud.ash.07@gmail.com>
2023-04-10 21:55:07 +0200

    add basic layut UI component

---
+++ src/UI/LayoutWrapper/LayoutWrapper.js
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+
+const LayoutWrapper = () => {
+    return (
+        <div className="layout-wrapper">
+            <Header />
+            Hello
+            <Outlet></Outlet>
+            <Footer />
+        </div>
+    )
+}
+
+export default LayoutWrapper;
