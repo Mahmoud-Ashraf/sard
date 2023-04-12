b9e190ce80de96b1cc53eb87dccffe3d36135dd2
shehab khallaf <shehabkhallaf122@gmail.com>
2023-04-12 20:38:35 +0200

    edit header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 // import { useState } from 'react';
-import {  NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     const logo = require('../../assets/images/logo-red.webp');
@@ -28,7 +28,7 @@ const Header = () => {
 
                 </div>
             </div>
-            <div className="header-middle">
+            <div className="header-middle m-2">
                 <div className='container d-flex justify-content-between'>
                     <img className='header-middle-logo' src={logo} alt='sard logo' />
                     <div className='row gx-4 align-items-center'>
@@ -38,20 +38,22 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-            <div className="header-bottom container">
-                <div className='row gx-4 align-items-center'>
-                    <NavLink to='/home' activeclassname='active-link' className='col-auto'>أحدث الأخبار</NavLink>
-                    <NavLink to='/' activeclassname='active-link' className='col-auto'>الصفحة الرئيسية</NavLink>
-                    <NavLink to='/' activeclassname='active-link' className='col-auto'>محليات</NavLink>
-                    <NavLink to='/' activeclassname='active-link' className='col-auto'>تقارير وحوارات</NavLink>
-                    <NavLink to='/' activeclassname='active-link' className='col-auto'>اقتصادية</NavLink>
-                    <NavLink to='/' activeclassname='active-link' className='col-auto'>تقنية</NavLink>
-                    <NavLink to='/' activeclassname='active-link' className='col-auto'>دولي</NavLink>
-                    <NavLink to='/' activeclassname='active-link' className='col-auto'>مقالات</NavLink>
-                    <NavLink to='/' activeclassname='active-link' className='col-auto'>صحة</NavLink>
-                    <NavLink to='/' activeclassname='active-link' className='col-auto'>الفيديو</NavLink>
-                    <NavLink to='/' activeclassname='active-link' className='col-auto'>تويتر</NavLink>
-                    <NavLink to='/' activeclassname='active-link' className='col-auto'>اخر</NavLink>
+            <div className="header-bottom p-2">
+                <div className='container'>
+                    <div className='row gx-4 align-items-center'>
+                        <NavLink to='/home' activeclassname='active-link' className='col-auto'>أحدث الأخبار</NavLink>
+                        <NavLink to='/' activeclassname='active-link' className='col-auto'>الصفحة الرئيسية</NavLink>
+                        <NavLink to='/' activeclassname='active-link' className='col-auto'>محليات</NavLink>
+                        <NavLink to='/' activeclassname='active-link' className='col-auto'>تقارير وحوارات</NavLink>
+                        <NavLink to='/' activeclassname='active-link' className='col-auto'>اقتصادية</NavLink>
+                        <NavLink to='/' activeclassname='active-link' className='col-auto'>تقنية</NavLink>
+                        <NavLink to='/' activeclassname='active-link' className='col-auto'>دولي</NavLink>
+                        <NavLink to='/' activeclassname='active-link' className='col-auto'>مقالات</NavLink>
+                        <NavLink to='/' activeclassname='active-link' className='col-auto'>صحة</NavLink>
+                        <NavLink to='/' activeclassname='active-link' className='col-auto'>الفيديو</NavLink>
+                        <NavLink to='/' activeclassname='active-link' className='col-auto'>تويتر</NavLink>
+                        <NavLink to='/' activeclassname='active-link' className='col-auto'>اخر</NavLink>
+                    </div>
                 </div>
             </div>
 
