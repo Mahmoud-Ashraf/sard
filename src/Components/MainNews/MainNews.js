f2bf8f20ab4deb71a40cc4b0211d5d1b3ce19064
Mahmoud Ashraf Taha <mahmoud.ash.07@gmail.com>
2023-04-13 01:03:45 +0200

    adjust responsive for main news structure

--- src/Components/MainNews/MainNews.js
+++ src/Components/MainNews/MainNews.js
@@ -8,16 +8,16 @@ const MainNews = () => {
     const mainNewsArr = useSelector(state => state.news[newsType]);
     return (
         <HomeSection title={'titles.' + newsType}>
-            <div className="row gx-2">
-                <div className="col-7">
+            <div className="row g-2">
+                <div className="col-lg-7">
                     <NewsBlock />
                 </div>
-                <div className="col-5">
+                <div className="col-lg-5">
                     <div className="row g-2">
-                        <div className="col-6"><NewsBlock size="sm" /></div>
-                        <div className="col-6"><NewsBlock size="sm" /></div>
-                        <div className="col-6"><NewsBlock size="sm" /></div>
-                        <div className="col-6"><NewsBlock size="sm" /></div>
+                        <div className="col-md-6"><NewsBlock size="sm" /></div>
+                        <div className="col-md-6"><NewsBlock size="sm" /></div>
+                        <div className="col-md-6"><NewsBlock size="sm" /></div>
+                        <div className="col-md-6"><NewsBlock size="sm" /></div>
                     </div>
                     
                 </div>
