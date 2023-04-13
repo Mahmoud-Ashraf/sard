75089852ab1dcbd55c70ba4eb65d8bba925963be
shehab khallaf <shehabkhallaf122@gmail.com>
2023-04-13 14:45:11 +0200

    Apps bar

---
+++ src/Components/DownloadApp/DownloadApp.js
@@ -0,0 +1,19 @@
+const logo = require('../../assets/images/logo-red.webp');
+const apps = require('../../assets/images/Apps.webp');
+
+
+const DownloadApp = () => {
+    return (
+        <div className='header-middle d-flex justify-content-between p-3'>
+            <div>
+                <img className='header-middle-logo' src={logo} alt='' />
+            </div>
+            <div>
+                <h4 className='text-center'>قم بتحميل تطبيق سرد الأخبارى الأن</h4>
+                <img src={apps} alt='' />
+            </div>
+
+        </div>
+    )
+}
+export default DownloadApp;
