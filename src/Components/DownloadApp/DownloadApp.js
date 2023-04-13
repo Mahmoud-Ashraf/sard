8d175532fc6b17e524800b9f4349ec9e37b31d91
shehab khallaf <shehabkhallaf122@gmail.com>
2023-04-13 14:46:27 +0200

    edit app style

--- src/Components/DownloadApp/DownloadApp.js
+++ src/Components/DownloadApp/DownloadApp.js
@@ -5,7 +5,7 @@ const apps = require('../../assets/images/Apps.webp');
 const DownloadApp = () => {
     return (
         <div className='header-middle d-flex justify-content-between p-3'>
-            <div>
+            <div className='d-flex align-items-center'>
                 <img className='header-middle-logo' src={logo} alt='' />
             </div>
             <div>
