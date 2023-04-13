0b3fb0bbe58c2e0ee012de1c314e2230fcfb0dfb
shehab khallaf <shehabkhallaf122@gmail.com>
2023-04-13 17:58:04 +0200

    create prayer box

--- src/Components/PrayerTime/PrayerTime.js
+++ src/Components/PrayerTime/PrayerTime.js
@@ -3,13 +3,51 @@ import Advertising from "../Advertising/Advertising";
 const PrayerTime = () => {
     return (
         <div className="prayer">
-            <h3>مواقيت الصلاة</h3>
+            <h3 className="prayer-header">مواقيت الصلاة</h3>
             <div className="row">
-                <div className="col">
+                <div className="col prayer-content">
+                    <div className="row justify-content-between">
+                        <div className="col ">السعودية,
+                            الرياض</div>
+                        <div className="col">8 فبراير
+                            2023 ميلادي</div>
+                    </div>
+                    <div className="row text-center prayer-content-times">
+                        <div className="col">
+                            <i class="fa-solid fa-moon"></i>
+                            <h5>الفجْر</h5>
+                            <p>٥:١٢ ص</p>
+                        </div>
+                        <div className="col">
+                            <i class="fa-solid fa-sun sun"></i>
+                             <h5>الشروق</h5>
+                            <p>٥:١٢ ص</p>
+                        </div>
+                        <div className="col">
+                            <i class="fa-solid fa-sun sun"></i>
+                             <h5>الظُّهْر</h5>
+                            <p>٥:١٢ ص</p>
+                        </div>
+                        <div className="col">
+                            <i class="fa-solid fa-sun sun"></i>
+                             <h5>العَصر</h5>
+                            <p>٥:١٢ ص</p>
 
+                        </div>
+                        <div className="col">
+                            <i class="fa-solid fa-cloud-sun sun"></i>
+                             <h5>المَغرب</h5>
+                            <p>٥:١٢ ص</p>
+                        </div>
+                        <div className="col">
+                            <i class="fa-solid fa-moon"></i>
+                             <h5>العِشاء</h5>
+                            <p>٥:١٢ ص</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="col">
-                    <Advertising/>
+                    <Advertising />
                 </div>
 
 
