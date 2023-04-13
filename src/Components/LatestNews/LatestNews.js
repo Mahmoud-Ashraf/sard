af9de22c59673071f0f8d1f47aba92bd5c7c78b8
shehab khallaf <shehabkhallaf122@gmail.com>
2023-04-13 13:37:46 +0200

    edit style of latest news

--- src/Components/LatestNews/LatestNews.js
+++ src/Components/LatestNews/LatestNews.js
@@ -8,39 +8,39 @@ const LatestNews = () => {
                 <div className="latest-news-main-header">
                     <div className="card mb-3" >
                         <div className="row g-0">
-                            <div className="col-md-4">
+                            <div className="col-md-3">
                                 <img src={ronaldo} className="img-fluid rounded-start" alt="..."/>
                             </div>
-                            <div className="col-md-8">
+                            <div className="col-md-9">
                                 <div className="card-body">
                                     <p className="card-text">وليد الركراكي ضمن المرشحين لتدريب الأخضر.</p>
-                                    <p className="card-text"><small className="text-body-secondary">Lمنذ 1 ساعة</small></p>
+                                    <p className="card-text"><small className="text-body-secondary">منذ 1 ساعة</small></p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="card mb-3" >
                         <div className="row g-0">
-                            <div className="col-md-4">
+                            <div className="col-md-3">
                                 <img src={ronaldo} className="img-fluid rounded-start" alt="..."/>
                             </div>
-                            <div className="col-md-8">
+                            <div className="col-md-9">
                                 <div className="card-body">
                                     <p className="card-text">وليد الركراكي ضمن المرشحين لتدريب الأخضر.</p>
-                                    <p className="card-text"><small className="text-body-secondary">Lمنذ 1 ساعة</small></p>
+                                    <p className="card-text"><small className="text-body-secondary">منذ 1 ساعة</small></p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="card mb-3" >
                         <div className="row g-0">
-                            <div className="col-md-4">
+                            <div className="col-md-3">
                                 <img src={ronaldo} className="img-fluid rounded-start" alt="..."/>
                             </div>
-                            <div className="col-md-8">
+                            <div className="col-md-9">
                                 <div className="card-body">
                                     <p className="card-text">وليد الركراكي ضمن المرشحين لتدريب الأخضر.</p>
-                                    <p className="card-text"><small className="text-body-secondary">Lمنذ 1 ساعة</small></p>
+                                    <p className="card-text"><small className="text-body-secondary">منذ 1 ساعة</small></p>
                                 </div>
                             </div>
                         </div>
