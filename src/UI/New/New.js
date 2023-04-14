6bacb464b0724d67528adb0b3626b953eb5b39ac
shehab khallaf <shehabkhallaf122@gmail.com>
2023-04-15 01:55:27 +0200

    try to remove padding from main-box in new component

--- src/UI/New/New.js
+++ src/UI/New/New.js
@@ -3,11 +3,11 @@ import UrgentNews from "../../Components/UrgentNews/UrgentNews";
 
 const News = () =>{
     return(
-        <div className="row">
-            <div className="col">
+        <div className="row g-1 bg-white">
+            <div className="col-lg-7">
                 <UrgentNews/>
             </div>
-            <div className="col">
+            <div className="col-lg-5">
                 <LatestNews/>
             </div>
         </div>
