85f6fbcfd8820218ccb00d47056ab27fc9dd2cda
shehab khallaf <shehabkhallaf122@gmail.com>
2023-04-14 02:18:06 +0200

    edit i from class to className

--- src/Components/PrayerTime/PrayerTime.js
+++ src/Components/PrayerTime/PrayerTime.js
@@ -15,33 +15,33 @@ const PrayerTime = () => {
                         </div>
                         <div className="row text-center prayer-content-times">
                             <div className="col">
-                                <i class="fa-solid fa-moon"></i>
+                                <i className="fa-solid fa-moon"></i>
                                 <h5>الفجْر</h5>
                                 <p>٥:١٢ ص</p>
                             </div>
                             <div className="col">
-                                <i class="fa-solid fa-sun sun"></i>
+                                <i className="fa-solid fa-sun sun"></i>
                                 <h5>الشروق</h5>
                                 <p>٥:١٢ ص</p>
                             </div>
                             <div className="col">
-                                <i class="fa-solid fa-sun sun"></i>
+                                <i className="fa-solid fa-sun sun"></i>
                                 <h5>الظُّهْر</h5>
                                 <p>٥:١٢ ص</p>
                             </div>
                             <div className="col">
-                                <i class="fa-solid fa-sun sun"></i>
+                                <i className="fa-solid fa-sun sun"></i>
                                 <h5>العَصر</h5>
                                 <p>٥:١٢ ص</p>
 
                             </div>
                             <div className="col">
-                                <i class="fa-solid fa-cloud-sun sun"></i>
+                                <i className="fa-solid fa-cloud-sun sun"></i>
                                 <h5>المَغرب</h5>
                                 <p>٥:١٢ ص</p>
                             </div>
                             <div className="col">
-                                <i class="fa-solid fa-moon"></i>
+                                <i className="fa-solid fa-moon"></i>
                                 <h5>العِشاء</h5>
                                 <p>٥:١٢ ص</p>
                             </div>
