d88cde6b0bc93b632dc158a1815b0780f4e82760
shehab khallaf <shehabkhallaf122@gmail.com>
2023-04-14 02:44:02 +0200

    add NewsBlock to urgent news

--- src/Components/UrgentNews/UrgentNews.js
+++ src/Components/UrgentNews/UrgentNews.js
@@ -1,12 +1,14 @@
 import HomeSection from "../../UI/HomeSection/HomeSection";
+import NewsBlock from "../../UI/NewsBlock/NewsBlock";
 const mainNew = require('../../assets/images/main-news.jpg');
 
 const UrgentNews = (props) => {
     return (
         <HomeSection title="titles.urgent" showAll="/">
-            <div className="urgent-news-main-header">
+            {/* <div className="urgent-news-main-header">
                 <img alt="" src={mainNew} />
-            </div>
+            </div> */}
+            <NewsBlock/>
         </HomeSection>
 
     )
