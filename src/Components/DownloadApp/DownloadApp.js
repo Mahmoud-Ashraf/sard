a9d1eb31f420b9a4b32772f957a28ec75d377b2f
shehab khallaf <shehabkhallaf122@gmail.com>
2023-04-27 14:45:38 +0200

    edit style

--- src/Components/DownloadApp/DownloadApp.js
+++ src/Components/DownloadApp/DownloadApp.js
@@ -1,19 +1,25 @@
+import HomeSection from "../../UI/HomeSection/HomeSection";
 const logo = require('../../assets/images/logo-red.webp');
 const apps = require('../../assets/images/Apps.webp');
 
 
 const DownloadApp = () => {
     return (
-        <div className='header-middle d-flex justify-content-between p-3'>
-            <div className='d-flex align-items-center'>
-                <img className='header-middle-logo' src={logo} alt='' />
-            </div>
-            <div>
-                <h4 className='text-center'>قم بتحميل تطبيق سرد الأخبارى الأن</h4>
-                <img src={apps} alt='' />
-            </div>
+        <HomeSection title="">
 
-        </div>
+
+            <div className='d-flex justify-content-between p-3'>
+
+                <div className='d-flex align-items-center'>
+                    <img className='header-middle-logo' src={logo} alt='' />
+                </div>
+                <div>
+                    <h4 className='text-center'>قم بتحميل تطبيق سرد الأخبارى الأن</h4>
+                    <img src={apps} alt='' />
+                </div>
+
+            </div>
+        </HomeSection>
     )
 }
 export default DownloadApp;
