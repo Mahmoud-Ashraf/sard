68085539f3254cd8ffd5691bc72fcd93cc5b0903
Mahmoud Ashraf Taha <mahmoud.ash.07@gmail.com>
2023-04-27 13:13:37 +0200

    add auth store

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import langReducer from "./Lang/Lang";
 import newsReducer from "./News/News";
+import authReducer from "./Auth/Auth";
 
 const store = configureStore({
-    reducer: { lang: langReducer, news: newsReducer }
+    reducer: { lang: langReducer, news: newsReducer, auth: authReducer }
 });
 
 export default store;
