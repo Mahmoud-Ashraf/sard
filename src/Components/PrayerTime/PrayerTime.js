fb6e0b7d914d0712c9f78dcc594cda145ea52efa
Mahmoud Ashraf Taha <mahmoud.ash.07@gmail.com>
2023-05-01 16:44:44 +0300

    add convert to arabic numbers method

--- src/Components/PrayerTime/PrayerTime.js
+++ src/Components/PrayerTime/PrayerTime.js
@@ -18,6 +18,11 @@ const PrayerTime = () => {
         }
     }
 
+    const ConvertToArabicNumbers = (num) => {
+        const arabicNumbers = '\u0660\u0661\u0662\u0663\u0664\u0665\u0666\u0667\u0668\u0669';
+        return new String(num).replace(/[0123456789]/g, (d) => { return arabicNumbers[d] });
+    }
+
     useEffect(() => {
         getPrayerTimes();
     }, [long, lat])
