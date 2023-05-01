14f0e67d50b4e143be480fffab70c0398933c1ce
Mahmoud Ashraf Taha <mahmoud.ash.07@gmail.com>
2023-05-02 01:25:15 +0300

    add route to single news

--- src/Components/SingleNews/SingleNews.js
+++ src/Components/SingleNews/SingleNews.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import NewsBlock from "../../UI/NewsBlock/NewsBlock";
 
 const SingleNews = (props) => {
@@ -17,8 +18,10 @@ const SingleNews = (props) => {
                 </div>
                 <div className="col-8">
                     <div className="single-news-details">
-                        <h3>{props.singleNews.title}</h3>
-                        <p>{props.singleNews.content.substring(0, 100)} ...</p>
+                        <Link to={`/news/details/${props.singleNews?.id}`}>
+                            <h3>{props.singleNews.title}</h3>
+                            <p>{props.singleNews.content.substring(0, 100)} ...</p>
+                        </Link>
                         <span>{date.toLocaleDateString('ar-SA', { weekday: 'long', ...options }) + ' هجريا,' + date.toLocaleDateString('ar-EG', options)}</span>
                     </div>
                 </div>
