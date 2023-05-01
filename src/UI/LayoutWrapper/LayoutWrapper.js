190f4d7b47f296abc6b7cc91fb972309f14124e9
Mahmoud Ashraf Taha <mahmoud.ash.07@gmail.com>
2023-05-01 16:08:35 +0300

    set location when app start

--- src/UI/LayoutWrapper/LayoutWrapper.js
+++ src/UI/LayoutWrapper/LayoutWrapper.js
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import useHTTP from "../../Hooks/use-http";
 import { authActions } from "../../Store/Auth/Auth";
 import { useEffect } from "react";
@@ -10,6 +10,7 @@ import Loader from "../../Components/Loader/Loader";
 const LayoutWrapper = () => {
     const dispatch = useDispatch();
     const { isLoading, sendRequest } = useHTTP();
+    const countryCode = useSelector(state => state.auth.countryCode);
     const getGuestToken = () => {
         if (localStorage.getItem('token') && localStorage.getItem('user')) {
             dispatch(authActions.setUser({ token: localStorage.getItem('token'), user: JSON.parse(localStorage.getItem('user')) }));
@@ -19,7 +20,7 @@ const LayoutWrapper = () => {
                 {
                     url: 'client/regist_guest',
                     method: 'POST',
-                    body: { country_shortcode: 'EG' }
+                    body: { country_shortcode: countryCode }
                 },
                 data => {
                     dispatch(authActions.setUser({ token: data.data.api_token, user: data.data }));
@@ -31,10 +32,21 @@ const LayoutWrapper = () => {
             )
         }
     }
+
+    const getCurrentLocation = () => {
+        fetch('https://ipapi.co/json/', { mehtod: 'GET', })
+            .then(res => res.json())
+            .then(data => {
+                dispatch(authActions.setLocation({ long: data.longitude, lat: data.latitude, countryCode: data.country_code }))
+            });
+    }
+    useEffect(() => {
+        getCurrentLocation();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     useEffect(() => {
         getGuestToken();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, [countryCode])
     return (
         isLoading ?
             <Loader />
