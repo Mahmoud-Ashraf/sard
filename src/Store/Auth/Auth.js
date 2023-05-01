b1e832e5d27637ed21a63ba6fa029fe66fcc041f
Mahmoud Ashraf Taha <mahmoud.ash.07@gmail.com>
2023-05-01 16:07:45 +0300

    implement lattitude, longitude and country code

--- src/Store/Auth/Auth.js
+++ src/Store/Auth/Auth.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialAuthState = { token: '', user: {} };
+const initialAuthState = { token: '', user: {}, long: '', lat: '', countryCode: '' };
 
 const authSlice = createSlice({
     name: 'auth',
@@ -9,6 +9,11 @@ const authSlice = createSlice({
         setUser(state, action) {
             state.token = action.payload.token || state.token;
             state.user = action.payload.user || state.user;
+        },
+        setLocation(state, action) {
+            state.long = action.payload.long;
+            state.lat = action.payload.lat;
+            state.countryCode = action.payload.countryCode
         }
     }
 })
