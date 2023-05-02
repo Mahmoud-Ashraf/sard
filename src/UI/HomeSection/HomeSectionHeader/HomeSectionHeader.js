507a629f5d53a5709a3fb8a1a17a8f014c46008c
Mahmoud taha <mahmoud.taha@idemia.com>
2023-05-02 16:51:52 +0300

    remove header div if no header

--- src/UI/HomeSection/HomeSectionHeader/HomeSectionHeader.js
+++ src/UI/HomeSection/HomeSectionHeader/HomeSectionHeader.js
@@ -3,8 +3,9 @@ import Translate from "../../../helpers/Translate/Translate"
 
 const HomeSectionHeder = (props) => {
     return (
+        (props.title || props.showAll) &&
         <header className="home-section-header">
-            <h2 className="home-section-header-title fw-bold"><Translate id={props.title} /></h2>
+            {props.title && <h2 className="home-section-header-title fw-bold"><Translate id={props.title} /></h2>}
             {props.showAll && <Link className="home-section-header-showall" to={props.showAll}><Translate id="showAll" /></Link>}
         </header>
     )
