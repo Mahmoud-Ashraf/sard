6125cfaa808b7120a72b099b964c39b483768929
Mahmoud Ashraf Taha <mahmoud.ash.07@gmail.com>
2023-05-03 00:33:48 +0300

    edit Tabs UI

--- src/Components/LatestNews/LatestNews.js
+++ src/Components/LatestNews/LatestNews.js
@@ -1,72 +1,26 @@
 import { Link } from "react-router-dom";
 import HomeSection from "../../UI/HomeSection/HomeSection";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import { useState } from "react";
+import MainBox from "../../UI/MainBox/MainBox";
+import useTranslate from "../../Hooks/use-translate";
+import Translate from "../../helpers/Translate/Translate";
+
 const ronaldo = require('../../assets/images/ronaldo.png');
 
 const LatestNews = (props) => {
+    const [key, setKey] = useState('newest');
+
     return (
-        <>
-            <HomeSection title="titles.lastNews" showAll="/">
-                <div className="latest-news-main" style={{ maxHeight: props.maxHeight }}>
-                    <div className="latest-news-main-card" >
-                        <div className="row">
-                            <div className="col-3">
-                                <img src={ronaldo} alt="..." />
-                            </div>
-                            <div className="col-9">
-                                <div className="latest-news-main-card-body">
-                                    <Link className="card-text" to="/">
-                                        <h6>وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
-                                    </Link>
-                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="latest-news-main-card" >
-                        <div className="row">
-                            <div className="col-3">
-                                <img src={ronaldo} alt="..." />
-                            </div>
-                            <div className="col-9">
-                                <div className="latest-news-main-card-body">
-                                    <Link className="card-text" to="/">
-                                        <h6 >وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
-                                    </Link>
-                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="latest-news-main-card" >
-                        <div className="row">
-                            <div className="col-3">
-                                <img src={ronaldo} alt="..." />
-                            </div>
-                            <div className="col-9">
-                                <div className="latest-news-main-card-body">
-                                    <Link className="card-text" to="/">
-                                        <h6 >وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
-                                    </Link>
-                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="latest-news-main-card" >
-                        <div className="row">
-                            <div className="col-3">
-                                <img src={ronaldo} alt="..." />
-                            </div>
-                            <div className="col-9">
-                                <div className="latest-news-main-card-body">
-                                    <Link className="card-text" to="/">
-                                        <h6 >وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
-                                    </Link>
-                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <section className="tabs-section">
+            <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+                className="mb-3">
+
+                <Tab eventKey="newest" title={useTranslate(`titles.newest`)}>
                     <div className="latest-news-main-card" >
                         <div className="row">
                             <div className="col-3">
@@ -112,6 +66,9 @@ const LatestNews = (props) => {
                             </div>
                         </div>
                     </div>
+                </Tab>
+
+                <Tab eventKey="most_readed" title={useTranslate(`titles.most_readed`)}>
                     <div className="latest-news-main-card" >
                         <div className="row">
                             <div className="col-3">
@@ -127,9 +84,56 @@ const LatestNews = (props) => {
                             </div>
                         </div>
                     </div>
-                </div>
-            </HomeSection>
-        </>
+                    <div className="latest-news-main-card" >
+                        <div className="row">
+                            <div className="col-3">
+                                <img src={ronaldo} alt="..." />
+                            </div>
+                            <div className="col-9">
+                                <div className="latest-news-main-card-body">
+                                    <Link className="card-text" to="/">
+                                        <h6>وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
+                                    </Link>
+                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="latest-news-main-card" >
+                        <div className="row">
+                            <div className="col-3">
+                                <img src={ronaldo} alt="..." />
+                            </div>
+                            <div className="col-9">
+                                <div className="latest-news-main-card-body">
+                                    <Link className="card-text" to="/">
+                                        <h6>وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
+                                    </Link>
+                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="latest-news-main-card" >
+                        <div className="row">
+                            <div className="col-3">
+                                <img src={ronaldo} alt="..." />
+                            </div>
+                            <div className="col-9">
+                                <div className="latest-news-main-card-body">
+                                    <Link className="card-text" to="/">
+                                        <h6>وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
+                                    </Link>
+                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Tab>
+
+            </Tabs>
+            <Link className="home-section-header-showall tabs-section-header-showall" to={`/news/${key}`}><Translate id="showAll" /></Link>
+        </section>
     )
 }
 export default LatestNews;
