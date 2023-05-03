3b18fd4cec8d5604ff7292be5d4aedbd50bc778b
shehab khallaf <shehabkhallaf122@gmail.com>
2023-05-04 01:54:03 +0300

    get data from api

--- src/Components/LatestNews/LatestNews.js
+++ src/Components/LatestNews/LatestNews.js
@@ -1,132 +1,98 @@
+import useHTTP from "../../Hooks/use-http";
 import { Link } from "react-router-dom";
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
-import { useState } from "react";
-import useTranslate from "../../Hooks/use-translate";
 import Translate from "../../helpers/Translate/Translate";
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import useTranslate from "../../Hooks/use-translate";
+import Loader from "../Loader/Loader";
 
-const ronaldo = require('../../assets/images/ronaldo.png');
+
 
 const LatestNews = (props) => {
     const [key, setKey] = useState('newest');
+    const { isLoading, sendRequest } = useHTTP();
+    const token = useSelector(state => state.auth.token);
+    const [News, setNews] = useState([]);
+
+    const getNews = () => {
+        if (token) {
+            sendRequest(
+                {
+                    url: `get_home_newss?filter_type=${key}&paginate=7&page=1`,
+                    method: 'GET'
+                },
+                data => {
+                    setNews(data.data);
+                },
+                err => {
+
+                }
+            )
+        }
+    }
+    useEffect(() => {
+        getNews();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [token, key])
 
     return (
+
         <section className="tabs-section">
             <Tabs
                 id="controlled-tab-example"
                 activeKey={key}
                 onSelect={(k) => setKey(k)}
                 className="mb-3">
-
                 <Tab eventKey="newest" title={useTranslate(`titles.newest`)}>
-                    <div className="latest-news-main-card" >
-                        <div className="row">
-                            <div className="col-3">
-                                <img src={ronaldo} alt="..." />
-                            </div>
-                            <div className="col-9">
-                                <div className="latest-news-main-card-body">
-                                    <Link className="card-text" to="/">
-                                        <h6>وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
-                                    </Link>
-                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
+                    {News?.map(New => {
+                        return (
+                            isLoading ?
+                                <Loader />
+                                :
+                                <div className="latest-news-main-card" >
+                                    <div className="row">
+                                        <div className="col-3">
+                                            <img src={New.resource.logo} alt="..." />
+                                        </div>
+                                        <div className="col-9">
+                                            <div className="latest-news-main-card-body">
+                                                <Link className="card-text" to="/">
+                                                    <h6>{New.title ? "" : New.content }</h6>
+                                                </Link>
+                                                <p className="mt-3"><small className="text-body-secondary">{New.created_from}</small></p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="latest-news-main-card" >
-                        <div className="row">
-                            <div className="col-3">
-                                <img src={ronaldo} alt="..." />
-                            </div>
-                            <div className="col-9">
-                                <div className="latest-news-main-card-body">
-                                    <Link className="card-text" to="/">
-                                        <h6>وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
-                                    </Link>
-                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="latest-news-main-card" >
-                        <div className="row">
-                            <div className="col-3">
-                                <img src={ronaldo} alt="..." />
-                            </div>
-                            <div className="col-9">
-                                <div className="latest-news-main-card-body">
-                                    <Link className="card-text" to="/">
-                                        <h6>وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
-                                    </Link>
-                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        )
+                    })}
                 </Tab>
 
                 <Tab eventKey="most_readed" title={useTranslate(`titles.most_readed`)}>
-                    <div className="latest-news-main-card" >
-                        <div className="row">
-                            <div className="col-3">
-                                <img src={ronaldo} alt="..." />
-                            </div>
-                            <div className="col-9">
-                                <div className="latest-news-main-card-body">
-                                    <Link className="card-text" to="/">
-                                        <h6>وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
-                                    </Link>
-                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
+                    {News?.map(New => {
+                        return (
+                            isLoading ?
+                                <Loader />
+                                :
+                                <div className="latest-news-main-card" >
+                                    <div className="row">
+                                        <div className="col-3">
+                                            <img src={New.resource.logo} alt="..." />
+                                        </div>
+                                        <div className="col-9">
+                                            <div className="latest-news-main-card-body">
+                                                <Link className="card-text" to="/">
+                                                    <h6>{New.title ? "" : New.content}</h6>
+                                                </Link>
+                                                <p className="mt-3"><small className="text-body-secondary">{New.created_from}</small></p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="latest-news-main-card" >
-                        <div className="row">
-                            <div className="col-3">
-                                <img src={ronaldo} alt="..." />
-                            </div>
-                            <div className="col-9">
-                                <div className="latest-news-main-card-body">
-                                    <Link className="card-text" to="/">
-                                        <h6>وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
-                                    </Link>
-                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="latest-news-main-card" >
-                        <div className="row">
-                            <div className="col-3">
-                                <img src={ronaldo} alt="..." />
-                            </div>
-                            <div className="col-9">
-                                <div className="latest-news-main-card-body">
-                                    <Link className="card-text" to="/">
-                                        <h6>وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
-                                    </Link>
-                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="latest-news-main-card" >
-                        <div className="row">
-                            <div className="col-3">
-                                <img src={ronaldo} alt="..." />
-                            </div>
-                            <div className="col-9">
-                                <div className="latest-news-main-card-body">
-                                    <Link className="card-text" to="/">
-                                        <h6>وليد الركراكي ضمن المرشحين لتدريب الأخضر.</h6>
-                                    </Link>
-                                    <p className="mt-3"><small className="text-body-secondary">منذ 1 ساعة</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        )
+                    })}
                 </Tab>
 
             </Tabs>
