dba41ad5022c410f330424617d77d8eaaeb474d2
Mahmoud taha <mahmoud.taha@idemia.com>
2023-05-07 11:28:11 +0300

    add spacing for no data

--- src/Components/NoData/NoData.js
+++ src/Components/NoData/NoData.js
@@ -1,7 +1,7 @@
 const NoData = () => {
     return (
         <div className="np-data">
-            <h3 className="text-center fs-2 text-danger">عفواً لا يوجد نتائج</h3>
+            <h3 className="text-center fs-2 text-danger py-3">عفواً لا يوجد نتائج</h3>
         </div>
     )
 }
