332282a20d65f687d62e609ce644e6ba5047b42f
Mahmoud taha <mahmoud.taha@idemia.com>
2023-05-18 15:41:11 +0300

    implement verify mail api

--- src/Components/EmailVerify/EmailVerify.js
+++ src/Components/EmailVerify/EmailVerify.js
@@ -3,25 +3,44 @@ import Translate from '../../helpers/Translate/Translate';
 import OtpInput from 'react-otp-input';
 import { useDispatch } from 'react-redux';
 import { StepperActions } from '../../Store/Stepper/Stepper';
+import useHTTP from '../../Hooks/use-http';
+import { authActions } from '../../Store/Auth/Auth';
 
 const EmailVerify = (props) => {
     const dispatch = useDispatch();
+    const { sendRequest } = useHTTP();
     const [OTP, setOTP] = useState("");
-    const onNext = (e) => {
-        e.preventDefault();
+    const onNext = () => {
         dispatch(StepperActions.nextStep());
 
     }
     useEffect(() => {
         console.log(OTP);
-    }, [OTP])
+    }, [OTP]);
+
+    const verifyMail = (e) => {
+        e.preventDefault()
+        sendRequest(
+            {
+                url: 'client/verify_email_code',
+                method: 'POST',
+                body: { code: OTP }
+            },
+            data => {
+                dispatch(authActions.setUser({ token: data.data.api_token, user: data.data }));
+                localStorage.setItem('token', data.data.api_token);
+                localStorage.setItem('user', JSON.stringify(data.data));
+                onNext();
+            }
+        )
+    }
     return (
         <div className="email-verify">
             <header>
                 <h2 className="step-header"><Translate id="titles.email-verify" /></h2>
             </header>
 
-            <form className='email-verify-form'>
+            <form className='email-verify-form' onSubmit={verifyMail}>
                 <div className='row flex-column'>
                     <div className='col-md-6'>
                         <OtpInput
@@ -33,7 +52,7 @@ const EmailVerify = (props) => {
                         />
                     </div>
                     <div className='col-md-3 mt-5'>
-                        <button className='w-100 main-button' type='submit' onClick={onNext}>التالي</button>
+                        <button className='w-100 main-button' type='submit'>التالي</button>
                     </div>
                 </div>
             </form>
--- src/Views/Auth/Auth.js
+++ src/Views/Auth/Auth.js
@@ -3,16 +3,20 @@ import MainBox from '../../UI/MainBox/MainBox';
 import Register from '../../Components/Register/Register';
 import EmailVerify from '../../Components/EmailVerify/EmailVerify';
 import Stepper from '../../UI/Stepper/Stepper';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { StepperActions } from '../../Store/Stepper/Stepper';
 import { authActions } from '../../Store/Auth/Auth';
 import SelectCountries from '../../Components/SelectCountries/SelectCountries';
 import SelectCategories from '../../Components/SelectCategories/SelectCategories';
 import SelectResourcesType from '../../Components/SelectResourcesType/SelectResourcesType';
 import SelectResources from '../../Components/SelectResources/SelectResources';
+import useHTTP from '../../Hooks/use-http';
 
 const Auth = () => {
     const dispatch = useDispatch();
+    const { sendRequest } = useHTTP();
+    const countryCode = useSelector(state => state.auth.countryCode);
+
     useEffect(() => {
         getCurrentLocation();
         dispatch(StepperActions.setSteps(
@@ -26,13 +30,40 @@ const Auth = () => {
             ]
         ));
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        getGuestToken();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [countryCode]);
+
     const getCurrentLocation = () => {
         fetch('https://ipapi.co/json/', { mehtod: 'GET' })
             .then(res => res.json())
             .then(data => {
                 dispatch(authActions.setLocation({ long: data.longitude, lat: data.latitude, countryCode: data.country_code }))
             });
+    }
+    const getGuestToken = () => {
+        if (localStorage.getItem('token') && localStorage.getItem('user')) {
+            dispatch(authActions.setUser({ token: localStorage.getItem('token'), user: JSON.parse(localStorage.getItem('user')) }));
+        }
+        else {
+            sendRequest(
+                {
+                    url: 'client/regist_guest',
+                    method: 'POST',
+                    body: { country_shortcode: countryCode }
+                },
+                data => {
+                    dispatch(authActions.setUser({ token: data.data.api_token, user: data.data }));
+                    localStorage.setItem('token', data.data.api_token);
+                    localStorage.setItem('user', JSON.stringify(data.data));
+                },
+                err => {
+                }
+            )
+        }
     }
     return (
         <div className="auth-section">
